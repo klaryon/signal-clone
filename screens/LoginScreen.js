003a15217e7da436108af30bb11f434ad03b3d81
klaryon <klary.ardon@gmail.com>
2021-04-23 12:20:36 +0200

    status bar

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,14 +1,17 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Button, Input, Image } from "react-native-elements";
+import { StatusBar } from "expo-status-bar";
 
 const LoginScreen = () => {
-    return (
-        <View>
-            <Text>I'm the login screen</Text>
-        </View>
-    )
-}
+  return (
+      <View>
+          <StatusBar style="light" />
+      <Text>I'm the login screen</Text>
+    </View>
+  );
+};
 
-export default LoginScreen
+export default LoginScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
