338d1a7c42710d9f809c7d39d9cbc2b0b643a43a
klaryon <klary.ardon@gmail.com>
2021-04-23 15:00:42 +0200

    backgroundcolor fix

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -50,7 +50,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     padding: 10,
-    backgrounColor: "white",
+    backgroundColor: "white",
   },
   inputContainer: {
     width: 300,
