a119ed78f25ae1b8ea798d53ff81222f5f547337
klaryon <klary.ardon@gmail.com>
2021-04-23 10:55:14 +0200

    initial setup

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDiWIH5oqfCtlFwNHdc9KXfGp0BWmacvqg",
+  authDomain: "signal-clone-dc6ae.firebaseapp.com",
+  projectId: "signal-clone-dc6ae",
+  storageBucket: "signal-clone-dc6ae.appspot.com",
+  messagingSenderId: "1072779212136",
+  appId: "1:1072779212136:web:c96ee8b380a97ca7b252dd"
+};
