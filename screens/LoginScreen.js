0bb6324dd9a90881bbd4123568b184a652e3072e
klaryon <klary.ardon@gmail.com>
2021-04-23 13:44:12 +0200

    email and password inputs

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,17 +1,32 @@
-import React from "react";
+import React,  {useState} from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Button, Input, Image } from "react-native-elements";
 import { StatusBar } from "expo-status-bar";
 
 const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-      <View>
-          <StatusBar style="light" />
-      <Text>I'm the login screen</Text>
+    <View>
+      <StatusBar style="light" />
+      <Image
+        source={{
+          uri:
+            "https://blog.mozilla.org/internetcitizen/files/2018/08/signal-logo.png",
+        }}
+        style={{ width: 200, height: 200 }}
+      />
+      <View style={styles.inputContainer}>
+        <Input placeholder="Email" autoFocus type="email" value={email} onChangeText={text => setEmail(text)} />
+              <Input placeholder="Password" secureTextEntry type="password" value={password} onChangeText={text => setPassword(text)} />
+      </View>
     </View>
   );
 };
 
 export default LoginScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  inputContainer: {},
+});
