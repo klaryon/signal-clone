af7d3b56d1bf1946c8712f9ee33877c5c5854766
klaryon <klary.ardon@gmail.com>
2021-04-24 21:42:21 +0200

    register screen navigation

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -3,7 +3,7 @@ import { KeyboardAvoidingView, StyleSheet, Text, View } from "react-native";
 import { Button, Input, Image } from "react-native-elements";
 import { StatusBar } from "expo-status-bar";
 
-const LoginScreen = () => {
+const LoginScreen = ({ navigation }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -36,7 +36,12 @@ const LoginScreen = () => {
         />
       </View>
       <Button containerStyle={styles.button} onPress={signIn} title="Login" />
-      <Button containerStyle={styles.button} type="outline" title="Register" />
+      <Button
+        onPress={() => navigation.navigate("Register")}
+        containerStyle={styles.button}
+        type="outline"
+        title="Register"
+      />
       <View style={{ height: 100 }} />
     </KeyboardAvoidingView>
   );
