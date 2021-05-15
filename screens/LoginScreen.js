dc9876e05e1965d00bbf15d3be16f7f77b8cd707
klaryon <klary.ardon@gmail.com>
2021-05-15 22:54:41 +0200

    useEffect unsubscribe loginScreen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,11 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { KeyboardAvoidingView, StyleSheet, Text, View } from "react-native";
 import { Button, Input, Image } from "react-native-elements";
 import { StatusBar } from "expo-status-bar";
+import { auth } from "../firebase";
 
 const LoginScreen = ({ navigation }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        navigation.replace("Home");
+      }
+    });
+
+    return unsubscribe;
+  }, []);
 
   const signIn = () => {};
 
@@ -14,8 +25,7 @@ const LoginScreen = ({ navigation }) => {
       <StatusBar style="light" />
       <Image
         source={{
-          uri:
-            "https://blog.mozilla.org/internetcitizen/files/2018/08/signal-logo.png",
+          uri: "https://blog.mozilla.org/internetcitizen/files/2018/08/signal-logo.png",
         }}
         style={{ width: 200, height: 200 }}
       />
--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,4 +1,4 @@
-import React, {useState, useLayoutEffect} from "react";
+import React, { useState, useLayoutEffect } from "react";
 import { StyleSheet, View } from "react-native";
 import { KeyboardAvoidingView } from "react-native";
 import { Button, Input, Text } from "react-native-elements";
@@ -11,8 +11,7 @@ const RegisterScreen = ({ navigation }) => {
   const [password, setPassword] = useState("");
   const [imageUrl, setImageUrl] = useState("");
 
-  useLayoutEffect(() =>
-  {
+  useLayoutEffect(() => {
     navigation.setOptions({
       headerBackTitle: "Back to Login",
     });
@@ -69,9 +68,12 @@ const RegisterScreen = ({ navigation }) => {
           onSubmitEditing={register}
         />
       </View>
-      
-      <Button containerStyle={styles.button} raised
-        onPress={register} title="Register"
+
+      <Button
+        containerStyle={styles.button}
+        raised
+        onPress={register}
+        title="Register"
       />
 
       <View style={{ height: 100 }} />
