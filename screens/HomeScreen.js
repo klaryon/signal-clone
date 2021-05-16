5fe57bc574a18979310000078d018db5d6eb5e6b
klaryon <klary.ardon@gmail.com>
2021-05-16 22:03:46 +0200

    headerRight homeScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,6 +2,7 @@ import React, { useLayoutEffect } from "react";
 import { TouchableOpacity } from "react-native";
 import { StyleSheet, ScrollView, SafeAreaView, View } from "react-native";
 import { Avatar } from "react-native-elements/dist/avatar/Avatar";
+import { AntDesign, SimpleLineIcons } from "@expo/vector-icons";
 import CustomListItem from "../components/CustomListitem";
 import { auth, db } from "../firebase";
 
@@ -25,8 +26,25 @@ const HomeScreen = ({ navigation }) => {
           </TouchableOpacity>
         </View>
       ),
+      headerRight: () => (
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            width: 80,
+            marginRight: 20,
+          }}
+        >
+          <TouchableOpacity activeOpacity={0.5}>
+            <AntDesign name="camerao" size={24} color="black" />
+          </TouchableOpacity>
+          <TouchableOpacity activeOpacity={0.5}>
+            <SimpleLineIcons name="pencil" size={24} color="black" />
+          </TouchableOpacity>
+        </View>
+      ),
     });
-  }, []);
+  }, [navigation]);
 
   return (
     <SafeAreaView>
