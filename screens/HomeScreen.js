4b236030399910bddc78b4e610c7b64c87227803
klaryon <klary.ardon@gmail.com>
2021-05-16 21:32:55 +0200

    avatar homeScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,31 @@
-import React from 'react'
-import { SafeAreaView } from 'react-native'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { StyleSheet, ScrollView, SafeAreaView, View } from 'react-native'
+import { Avatar } from 'react-native-elements/dist/avatar/Avatar'
+import CustomListItem from '../components/CustomListitem'
+import { auth,db } from "../firebase";
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) =>
+{
+    useLayoutEffect(() =>
+    {
+        navigation.setOptions({
+            title: 'Signal',
+            headerStyle: { backgroundColor: "#fff" },
+            headerTitleStyle: { color: "black" },
+            headerTintColor: "black",
+            headerLeft: () => (
+                <View style={{ marginLeft: 20 }}>
+                    <Avatar rounded source={{ uri: auth?.currentUser?.photoURL }} />
+                </View>
+            )
+        });
+    }, [])
+
     return (
         <SafeAreaView>
-            <Text>This is the homepage</Text>
+            <ScrollView>
+                <CustomListItem />
+            </ScrollView>
         </SafeAreaView>
     )
 }
