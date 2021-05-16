ebcc43c6a4a96fa0ab170fb22207f27bed862de7
klaryon <klary.ardon@gmail.com>
2021-05-16 18:31:46 +0200

    safeAreaView

---
+++ screens/HomeScreen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { SafeAreaView } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
+
+const HomeScreen = () => {
+    return (
+        <SafeAreaView>
+            <Text>This is the homepage</Text>
+        </SafeAreaView>
+    )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({})
