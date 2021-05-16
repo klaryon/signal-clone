39312c3e5057d4c90346a71c6b6c9e4f41c43a02
klaryon <klary.ardon@gmail.com>
2021-05-16 21:51:55 +0200

    signin signout functions

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,38 +1,42 @@
-import React, { useLayoutEffect } from 'react'
-import { TouchableOpacity } from 'react-native';
-import { StyleSheet, ScrollView, SafeAreaView, View } from 'react-native'
-import { Avatar } from 'react-native-elements/dist/avatar/Avatar'
-import CustomListItem from '../components/CustomListitem'
-import { auth,db } from "../firebase";
+import React, { useLayoutEffect } from "react";
+import { TouchableOpacity } from "react-native";
+import { StyleSheet, ScrollView, SafeAreaView, View } from "react-native";
+import { Avatar } from "react-native-elements/dist/avatar/Avatar";
+import CustomListItem from "../components/CustomListitem";
+import { auth, db } from "../firebase";
 
-const HomeScreen = ({ navigation }) =>
-{
-    useLayoutEffect(() =>
-    {
-        navigation.setOptions({
-            title: 'Signal',
-            headerStyle: { backgroundColor: "#fff" },
-            headerTitleStyle: { color: "black" },
-            headerTintColor: "black",
-            headerLeft: () => (
-                <View style={{ marginLeft: 20 }}>
-                    <TouchableOpacity>
-                        <Avatar rounded source={{ uri: auth?.currentUser?.photoURL }} />
-                    </TouchableOpacity>
-                </View>
-            )
-        });
-    }, [])
+const HomeScreen = ({ navigation }) => {
+  const signOutUser = () => {
+    auth.signOut().then(() => {
+      navigation.replace("Login");
+    });
+  };
 
-    return (
-        <SafeAreaView>
-            <ScrollView>
-                <CustomListItem />
-            </ScrollView>
-        </SafeAreaView>
-    )
-}
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: "Signal",
+      headerStyle: { backgroundColor: "#fff" },
+      headerTitleStyle: { color: "black" },
+      headerTintColor: "black",
+      headerLeft: () => (
+        <View style={{ marginLeft: 20 }}>
+          <TouchableOpacity onPress={signOutUser} activeOpacity={0.5}>
+            <Avatar rounded source={{ uri: auth?.currentUser?.photoURL }} />
+          </TouchableOpacity>
+        </View>
+      ),
+    });
+  }, []);
 
-export default HomeScreen
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <CustomListItem />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
-const styles = StyleSheet.create({})
+export default HomeScreen;
+
+const styles = StyleSheet.create({});
