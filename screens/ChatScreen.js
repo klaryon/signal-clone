5033f4d011f26006cfe60841002d44767d002ecf
Clarissa Ardon <clarissa.ardon@flykube.com>
2021-06-06 18:41:53 +0200

    initial avatar on chatScreen component

---
+++ screens/ChatScreen.js
@@ -0,0 +1,33 @@
+import React, { useLayoutEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Avatar } from "react-native-elements";
+
+const ChatScreen = ({ navigation, route }) => {
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: "Chat",
+      headerBackTitleVisible: false,
+      headerTitleAlign: "left",
+      headerTitle: () => (
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <Avatar
+            rounded
+            source={{
+              uri: "https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png",
+            }}
+          />
+          <Text>{route.params.chatName}</Text>
+        </View>
+      ),
+    });
+  }, [navigation]);
+  return (
+    <View>
+      <Text>{route.params.chatName}</Text>
+    </View>
+  );
+};
+
+export default ChatScreen;
+
+const styles = StyleSheet.create({});
