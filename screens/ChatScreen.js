de9b0db691955805bf396c70e7c2e37040213f80
Clarissa Ardon <clarissa.ardon@flykube.com>
2021-06-06 19:08:58 +0200

    headerLeft chatScreen

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,6 +1,7 @@
 import React, { useLayoutEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { TouchableOpacity, StyleSheet, Text, View } from "react-native";
 import { Avatar } from "react-native-elements";
+import { AntDesign, FontAwesome, Ionicons } from "@expo/vector-icons";
 
 const ChatScreen = ({ navigation, route }) => {
   useLayoutEffect(() => {
@@ -16,9 +17,14 @@ const ChatScreen = ({ navigation, route }) => {
               uri: "https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png",
             }}
           />
-          <Text>{route.params.chatName}</Text>
+          <Text style={{ color:"white", marginLeft: 10, fontWeight: "700" }}>{route.params.chatName}</Text>
         </View>
-      ),
+        ),
+        headerLeft: () => (
+        <TouchableOpacity style={{marginLeft: 10}} onPress={navigation.goBack}>
+            <AntDesign name="arrowleft" size={24} color="white" />
+        </TouchableOpacity>
+        )
     });
   }, [navigation]);
   return (
